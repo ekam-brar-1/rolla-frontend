7250690fc329f643cf9793d77fda93fb54008a7d
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-02-24 12:24:17 -0700

    xyz

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase Config from Environment Variables
+
 const fetchFirebaseConfig = async () => {
   try {
-    const response = await fetch(
-      "https://rolla-backend.onrender.com/api/config"
-    ); // Replace with your deployed backend URL
+    const response = await fetch("http://localhost:5000/api/config"); // Replace with your deployed backend URL
     const config = await response.json();
     return config;
   } catch (error) {
@@ -16,8 +15,9 @@ const fetchFirebaseConfig = async () => {
   }
 };
 
+const firebaseConfig = await fetchFirebaseConfig();
 // Initialize Firebase
-const app = initializeApp(fetchFirebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Auth and Provider
 export const auth = getAuth(app);
